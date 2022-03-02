c23f1bc79edaac523eae8b3b9a88cf1cc9f14723
gustavo <guguines@hotmail.com>
2022-03-02 18:24:30 -0300

    Implementação da classe Main

---
+++ src/estudo/Main.java
@@ -0,0 +1,11 @@
+package estudo;
+
+import estudo.set.ClassSet;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassSet cs = new ClassSet();
+        cs.estudoMetodosSet();
+        cs.ordenacaoSet();
+    }
+}

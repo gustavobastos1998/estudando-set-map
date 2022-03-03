31610a0a7172b4b08788a1bcee9a14e01c07fd1e
gustavo <guguines@hotmail.com>
2022-03-02 22:25:41 -0300

    Classe para execução dos exercícios

---
+++ src/exercicio/ExercicioMap.java
@@ -0,0 +1,14 @@
+package exercicio;
+
+public class ExercicioMap {
+    public void executarExercicio(){
+        System.out.println("Crie e adicione ao dicionário a relação entre estado e população: ");
+        System.out.println();
+        System.out.println("Susbstitua a população de RN por 3,534,165: ");
+        System.out.println();
+        System.out.println("Confira se o estado de PB está presente, caso nao esteja insira-o com 4,039,277 de população:");
+        System.out.println();
+        System.out.println("Exiba a população de PE: ");
+        System.out.println();
+    }
+}
